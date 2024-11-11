05172b153ead6ae1385c2f8d6e7981995372223e
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-11-11 21:57:00 +0800

    style: remove carousel, add text on top of the image

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,7 +1,5 @@
-import { Carousel, Col, Container, Row, Image } from "react-bootstrap";
+import { Col, Container, Row, Image } from "react-bootstrap";
 import HomeNavbar from "./components/HomeNavbar";
-import carousel1 from '../../assets/carousel1.jpeg';
-import carousel2 from '../../assets/carousel2.jpeg';
 import barbershop from '../../assets/barbershop.jpg';
 import barbertools from '../../assets/barbertools.jpg';
 import barberpeople from '../../assets/barberpeople.jpg';
@@ -12,38 +10,30 @@ const Home = () => {
     return (
         <>
             <HomeNavbar />
-            <Container className="mt-5 mb-5">
-                <Carousel>
-                    <Carousel.Item >
-                        <img className="w-100" src={carousel1} alt="" />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="w-100" src={carousel2} alt="" />
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </Container>
-            <Container>
+            <Container className="mt-5">
                 <Row>
                     <Col className="p-1" md={4}>
                         <div className="h-100 image-container">
                             <Image className="h-100" src={barbershop} fluid rounded={true} />
                             <h4 className="overlay-text">Locations</h4>
                         </div>
-
                     </Col>
                     <Col className="p-1" md={4}>
-                        <Image className="mb-2" rounded={true} src={barbertools} fluid />
-                        <Image src={barberpeople} rounded={true} fluid />
+                        <div className="image-container">
+                            <Image className="mb-2" rounded={true} src={barbertools} fluid />
+                            <h4 className="overlay-text-2">Services</h4>
+                        </div>
+                        <div className="image-container">
+                            <Image src={barberpeople} rounded={true} fluid />
+                            <h4 className="overlay-text-2">About Us</h4>
+                        </div>
                     </Col>
-                    <Col className="p-1" md={4}><Image className="h-100" src={barbercutty} fluid rounded={true} /></Col>
+                    <Col className="p-1" md={4}>
+                        <div className="h-100 image-container">
+                            <Image className="h-100" src={barbercutty} fluid rounded={true} />
+                            <h4 className="overlay-text">Book Now</h4>
+                        </div>
+                    </Col>
 
                 </Row>
             </Container>
